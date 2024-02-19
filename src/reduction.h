a9cfa622aece61df1e609caa251619d09c134add
WanisC <wanischouaib@gmail.com>
2024-02-19 18:49:25 +0100

    Ajout d'une réduction (temporaire)

--- src/reduction.h
+++ src/reduction.h
@@ -5,14 +5,15 @@
 	Entrée: un chiffré
 	Sortie: "texte clair" dans une taille spécifique
 
-	Description: Notre fonction de réduction prendra les 6 premiers chiffres de notre chiffré
-
+	Description: notre fonction de réduction prendra les 6 premiers chiffres de notre chiffré
 */
 void reduction(char *chiffre, char *reduit);
 
 /* Fonction de recherche dans la table arc-en-ciel
 
 	Entrée: un mot de passe
-	Sortie: Pour tout couple (P1, P2), retourne True si le mot de passe est égal à P2, False sinon
+	Sortie: pour tout couple (P1, P2), retourne True si le mot de passe est égal à P2, False sinon
+
+	Description: recherche du mot de passe dans notre table arc-en-ciel
 */
 bool recherche(char *mdp, char *table);
