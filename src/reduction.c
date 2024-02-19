cf96c02d00b3697e275df6cf76aebe6d39cd0d21
WanisC <wanischouaib@gmail.com>
2024-02-19 18:24:55 +0100

    Ajout d'une réduction (temporaire)

--- src/reduction.c
+++ src/reduction.c
@@ -1,3 +1,20 @@
-#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
-void reduction(void) {}
+void reduction(char *chiffre, char *reduit) {	
+	int cpt=0;
+	for (int i=0; i<strlen(chiffre); i++) {
+		if (cpt != 7) {
+			if (isdigit(chiffre[i])) {
+				char temp[2] = {chiffre[i], '\0'};
+				strncat(reduit, temp, 2);
+				cpt++;
+			}
+		} else break;
+	}
+}
+
+bool recherche(char *mdp, char *table) {
+	return false;
+}
--- src/reduction.h
+++ src/reduction.h
@@ -1,7 +1,18 @@
+#include <stdbool.h>
+
 /* Fonction de réduction (R)
 
 	Entrée: un chiffré
 	Sortie: "texte clair" dans une taille spécifique
 
+	Description: Notre fonction de réduction prendra les 6 premiers chiffres de notre chiffré
+
 */
-void reduction(void);
+void reduction(char *chiffre, char *reduit);
+
+/* Fonction de recherche dans la table arc-en-ciel
+
+	Entrée: un mot de passe
+	Sortie: Pour tout couple (P1, P2), retourne True si le mot de passe est égal à P2, False sinon
+*/
+bool recherche(char *mdp, char *table);
