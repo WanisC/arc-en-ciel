c7a342c715701b5682de2c5b1755cfdbb0bc851c
WanisC <wanischouaib@gmail.com>
2024-02-19 18:24:23 +0100

    Ajout de SHA256 (temporaire)

--- src/hashage.c
+++ src/hashage.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sodium.h>
 
-void hash(void) {
-	printf("Not implemented.\n");
+void SHA256(char *mdp, unsigned char *hash) {
+	// Initialisation de la librairie
+	if (sodium_init() == -1) {
+		printf("Erreur lors de l'initialisation de libsodium\n");
+		exit(1);
+	}
+
+    crypto_hash_sha256(hash, (const unsigned char *)mdp, strlen(mdp));
 }
--- src/hashage.h
+++ src/hashage.h
@@ -3,5 +3,7 @@
 	Entrée: un mot de passe
 	Sortie: le chiffré du mot de passe en entrée
 
+	Description: 
+
 */
-void hash(void);
+void SHA256(char *mdp, unsigned char *hash);
