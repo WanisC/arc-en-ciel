08874f037552a35cb1f010b8643c1d9c84ec8698
WanisC <wanischouaib@gmail.com>
2024-02-19 18:49:09 +0100

    Ajout de SHA256 (temporaire)

--- src/hashage.h
+++ src/hashage.h
@@ -3,7 +3,7 @@
 	Entrée: un mot de passe
 	Sortie: le chiffré du mot de passe en entrée
 
-	Description: 
+	Description: utilise la fonction crypto_hash_sha256 de la librairie sodium.h pour chiffrer le mot de passe et stocker le résultat dans la variable hash 
 
 */
 void SHA256(char *mdp, unsigned char *hash);
