f2494bd8187b570df2b653732714f8b294da2840
Mathis78 <77025365+Mathis78@users.noreply.github.com>
2024-02-22 12:40:41 +0100

    delete all

--- src/hashage.h
+++
@@ -1,9 +0,0 @@
-/* Fonction de hashage (H)
-
-	Entrée: un mot de passe
-	Sortie: le chiffré du mot de passe en entrée
-
-	Description: utilise la fonction crypto_hash_sha256 de la librairie sodium.h pour chiffrer le mot de passe et stocker le résultat dans la variable hash 
-
-*/
-void SHA256(char *mdp, unsigned char *hash);
