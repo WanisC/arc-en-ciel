ba8926e6e933e9e2405cc216acb1fa8c4b4c966e
WanisC <wanischouaib@gmail.com>
2024-02-22 17:08:20 +0100

    Ajout de plusieurs fonctions de réduction

--- src/reduction.c
+++ src/reduction.c
@@ -1,19 +1,88 @@
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 
-void reduction(char *chiffre, char *reduit) {	
+void R1(char *chiffre, char *reduit) {	
 	int cpt=0;
 	for (int i=0; i<strlen(chiffre); i++) {
-		if (cpt != 7) {
+		if (cpt!=7) {
 			if (isdigit(chiffre[i])) {
-				char temp[2] = {chiffre[i], '\0'};
-				strncat(reduit, temp, 2);
+				char temp[2]={chiffre[i],'\0'};
+				strncat(reduit,temp,2);
 				cpt++;
 			}
 		} else break;
 	}
 }
+
+void R2(char *chiffre, char *reduit) {
+	int somme=0;
+	for (int i=0; i<strlen(chiffre); i++) {
+		if (chiffre[i]>='0' && chiffre[i]<='9') {
+			somme+=chiffre[i]-'0';
+		} else if (chiffre[i]>='a' && chiffre[i]<='f') {
+			somme+=chiffre[i]-'a'+10;
+		} else {
+			somme+=chiffre[i]-'A'+10;
+		}
+	}
+	sprintf(reduit,"%d",somme);
+}
+
+void R3(char *chiffre, char *reduit) {
+	int cpt=0;
+	for (int i=0; i<strlen(chiffre); i++) {
+		if (cpt!=7) {
+			if (isalpha(chiffre[i])) {
+				char temp[2]={chiffre[i],'\0'};
+				strncat(reduit,temp,2);
+				cpt++;
+			}
+		} else break;
+	}
+}
+
+void R4(char *chiffre, char *reduit) {
+	int cpt=0;
+	for (int i=0; i<strlen(chiffre); i++) {
+		if (cpt!=7) {
+			char temp[2]={chiffre[i],'\0'};
+			strncat(reduit,temp,2);
+			cpt++;
+		} else break;
+	}
+}
+
+void R5(char *chiffre, char *reduit) {
+	int somme=0;
+	for (int i=0; i<strlen(chiffre); i++) {
+		if (isdigit(chiffre[i])) {
+			somme+=chiffre[i];
+		}
+	}
+	sprintf(reduit,"%d",somme);
+}
+
+void R6(char *chiffre, char *reduit) {
+	int somme=0;
+	for (int i=0; i<strlen(chiffre); i++) {
+		if (chiffre[i]>='a' && chiffre[i]<='j') {
+			somme=chiffre[i]-'a'+10;
+		} else if (chiffre[i]>='A' && chiffre[i]<='J') {
+			somme=chiffre[i]-'A'+10;
+		} else continue;
+	}
+	sprintf(reduit,"%d",somme);
+}
+
+void R7(char *chiffre, char*reduit) {}
+
+void R8(char *chiffre, char *reduit) {}
+
+void R9(char *chiffre, char *reduit) {}
+
+void R10(char *chiffre, char *reduit) {}
 
 bool recherche(char *mdp, char *table) {
 	return false;
--- src/reduction.h
+++ src/reduction.h
@@ -1,19 +1,38 @@
 #include <stdbool.h>
 
-/* Fonction de réduction (R)
+/* Fonction de réduction (R1)
 
 	Entrée: un chiffré
 	Sortie: "texte clair" dans une taille spécifique
 
-	Description: notre fonction de réduction prendra les 6 premiers chiffres de notre chiffré
+	Description: 
+		R1  - ne récupère que les 7 premiers caractères numériques rencontrés
+		R2  - transforme le chiffré en base décimale
+		R3  - ne récupère que les 7 premiers caractères alphabétiques recontrés
+		R4  - ne récupère que les 7 premiers caractères rencontrés (alphabétique ou numérique)
+		R5  - 
+		R6  - 
+		R7  -
+		R8  -
+		R9  -
+		R10 -
 */
-void reduction(char *chiffre, char *reduit);
+void R1(char *chiffre, char *reduit);
+void R2(char *chiffre, char *reduit);
+void R3(char *chiffre, char *reduit);
+void R4(char *chiffre, char *reduit);
+void R5(char *chiffre, char *reduit);
+void R6(char *chiffre, char *reduit);
+void R7(char *chiffre, char *reduit);
+void R8(char *chiffre, char *reduit);
+void R9(char *chiffre, char *reduit);
+void R10(char *chiffre, char *reduit);
 
 /* Fonction de recherche dans la table arc-en-ciel
 
 	Entrée: un mot de passe
-	Sortie: pour tout couple (P1, P2), retourne True si le mot de passe est égal à P2, False sinon
+	Sortie: True ou False
 
-	Description: recherche du mot de passe dans notre table arc-en-ciel
+	Description: pour tout couple (P1,P2) retourne True si on trouve P2 dans notre table, False sinon
 */
 bool recherche(char *mdp, char *table);
