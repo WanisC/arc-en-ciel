f91d2876030b86c51b1d253ea5ac6bf9721ecd20
WanisC <wanischouaib@gmail.com>
2024-02-22 17:07:32 +0100

    Modifications mineures

--- src/hashage.c
+++ src/hashage.c
@@ -4,11 +4,11 @@
 #include <sodium.h>
 
 void SHA256(char *mdp, unsigned char *hash) {
-	// Initialisation de la librairie
+	// Initialisation de la librairie	
 	if (sodium_init() == -1) {
 		printf("Erreur lors de l'initialisation de libsodium\n");
 		exit(1);
 	}
 
-    crypto_hash_sha256(hash, (const unsigned char *)mdp, strlen(mdp));
+	crypto_hash_sha256(hash, (const unsigned char *)mdp, strlen(mdp));
 }
